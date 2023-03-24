d2f857b401a6ec1486cadf1320cd0e74e12a8d66
dpapadat94 <102779032+dpapadat94@users.noreply.github.com>
2023-03-24 16:38:07 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>React is Cool!</p>
+        <h1>First react Project</h1>
+        <p>List items below</p>
+        <ul>
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+        </ul>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
